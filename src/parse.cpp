4915dabcfd9418372ebe9f39197b145efc1363de
colinl <colinl@mox271.macguff.fr>
2014-12-05 16:19:17 +0100

    add delete node in file

--- src/parse.cpp
+++ src/parse.cpp
@@ -31,13 +31,13 @@ public :
 
 class PNode {
 public :
+	string class_type;
 	string name;
 	int first_line;
 	int last_line;
 	PNode(){}
-	PNode(string name, int fline) : name(name), first_line(fline){}
+	PNode(string class_type, int fline) : class_type(class_type), first_line(fline){}
 };
-
 
 void find_node(Tokken& tokken, bool& isgroup, bool& search_name,
 		vector<PNode>& list_node, int& n_line){
@@ -45,7 +45,7 @@ void find_node(Tokken& tokken, bool& isgroup, bool& search_name,
 		PNode node = PNode(tokken.first_word, n_line);
 		list_node.push_back(node);
 	}
-	if(tokken.first_word == "}"){
+	else if(tokken.first_word == "}"){
 		if(list_node.empty()){
 			return;
 		}
@@ -53,13 +53,19 @@ void find_node(Tokken& tokken, bool& isgroup, bool& search_name,
 			list_node[list_node.size()-1].last_line = n_line;
 		}
 	}
-	if(tokken.first_word == "Group"){
+	else if(tokken.first_word == "name"){
+		list_node[list_node.size()-1].name = tokken.second_word;
+	}
+	else if(tokken.first_word == "Group"){
 		isgroup = true;
 		search_name = true;
 	}
-	if(tokken.first_word == "end_group"){
+	else if(tokken.first_word == "end_group"){
 		isgroup = false;
 		search_name = false;
+	}
+	else{
+		return;
 	}
 }
 
@@ -81,18 +87,15 @@ void work_group(const string& line, bool& search_name, string& name){
 	}
 }
 */
-
-int main() {
-
+vector<PNode> parse_nk(const char* file){
 	ifstream infile;
-	infile.open("/u/colinl/testnk.nk");
+	infile.open(file);
 	string line;
 
 	bool isgroup = false;
 	bool search_name;
 
 	string name;
-	vector<string> list_name;
 	vector<PNode> list_node;
 	int n_line = 1;
 
@@ -101,16 +104,36 @@ int main() {
 		Tokken tokken = get_tokken(line);
 		find_node(tokken, isgroup, search_name, list_node, n_line);
 		n_line++;
-/*			if(isgroup){
-				work_group(line, search_name, name);
-				list_name.push_back(name);
-			}*/
 	}
-/*	set<string> set_name(list_name.begin(), list_name.end());
-	BOOST_FOREACH(string element, set_name) {
-		list_node.push_back(element);
-	}*/
+	return list_node;
+}
+
+void copy_line(const char* in_file, const char* out_file, PNode pnode)
+{
+	ifstream in_data;
+	in_data.open(in_file);
+	ofstream out_data;
+	out_data.open(out_file);
+	string line;
+	int n_line = 1;
+	int fline = pnode.first_line;
+	int lline = pnode.last_line;
+    while (getline(in_data, line))
+    {
+    	if(n_line < fline || n_line > lline){
+            out_data << line << endl;
+    	}
+    	n_line++;
+    }
+    remove(in_file);
+    rename(out_file, in_file);
+}
+
+
+int main() {
+	vector<PNode> list_node = parse_nk("/u/colinl/testnk.nk");
 	for(int i; i<int(list_node.size()); i++){
-		cout<<list_node[i].name<<" "<<list_node[i].first_line<<" "<<list_node[i].last_line<<endl;
+		cout<<list_node[i].name<<" "<<list_node[i].class_type<<" "<<list_node[i].first_line<<" "<<list_node[i].last_line<<endl;
 	}
+	copy_line("/u/colinl/testnk.nk", "/u/colinl/testnk.nk.new", list_node[0]);
 }
