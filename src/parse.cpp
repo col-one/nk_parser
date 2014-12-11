ac952e63ba1e2bef3a24bf8a66d0366d569daeaf
colinl <colinl@mox271.macguff.fr>
2014-12-11 20:12:22 +0100

    ok pour command pas encore proof

--- src/parse.cpp
+++ src/parse.cpp
@@ -8,9 +8,19 @@
 //============================================================================
 
 #include "tokken.h"
+#include <iostream>
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-	remove_nodes_nk("/u/colinl/testnk.nk", std::string("MgPreRender1"), false);
+	std::string node = argv[2];
+	std::string typen = "-node";
+	std::string arg = argv[3];
+	bool type = false;
+	if(arg.compare(typen)){
+		type = true;
+	}
+	std::cerr<<type;
+	const char* path = argv[1];
+	remove_nodes_nk(path, node, !type);
 	return 1;
 }
